61d28cdf61ecde15804975b282b4cb3b9e36fa67
monica hung <monicahung@users.noreply.github.com>
2021-12-06 04:56:13 +0000

    Day 3 part 2

--- Day3.rb
+++ Day3.rb
@@ -3,42 +3,83 @@ def day3
     num.chomp.split('').map(&:to_i)
   }
   # data looks like [[0, 0, 1, 0, 0], [0, 1, 1, 1, 0]]
-  # init
-  counts = []
-  (0..data[0].length-1).each do
-    counts.push({ 'ones' => 0, 'zeroes' => 0 })
-  end
 
-  # count all the occurrences
-  data.each_with_index do |val, index|
-    (0..data[0].length).each do |bit|
-      case data[index][bit]
-      when 0
-        counts[bit]['zeroes'] += 1
-      when 1
-        counts[bit]['ones'] += 1
-      end
-    end
-  end
-
-  # generate epsilon and gamma
-  most_common = []
-  least_common = []
-  counts.each do |counts|
-    if counts['ones'] > counts['zeroes']
-      most_common.push('1')
-      least_common.push('0')
-    else
-      # assumes never the same amount
-      most_common.push('0')
-      least_common.push('1')
-    end
-  end
+  oxygen = get_oxygen_value(data)
+  co2 = get_co2_val(data)
 
   # convert to decimal
-  num_common = most_common.join.to_i(2)
-  num_uncommon = least_common.join.to_i(2)
+  num_oxygen = oxygen.join.to_i(2)
+  num_co2 = co2.join.to_i(2)
 
   # answer
-  puts num_common * num_uncommon
+  puts num_oxygen * num_co2
+end
+
+def get_oxygen_value(data)
+  # init
+  counts = {'ones' => 0, 'zeroes' => 0 }
+  new_set_zeroes = []
+  new_set_ones = []
+
+  # count all the occurrences
+  (0..data[0].length).each do |bit|
+    data.each_with_index do |val, index|
+      case data[index][bit]
+      when 0
+        counts['zeroes'] += 1
+        new_set_zeroes.push(data[index])
+      when 1
+        counts['ones'] += 1
+        new_set_ones.push(data[index])
+      end
+    end
+    if counts['zeroes'] > counts['ones']
+      data = new_set_zeroes
+    else
+      data = new_set_ones # 1 is default for oxygen
+    end
+    if data.length == 1
+      return data[0]
+    end
+    #reset counts for next iter
+    new_set_zeroes = []
+    new_set_ones = []
+    counts = {'ones' => 0, 'zeroes' => 0 }
+  end
+  return data[0]
+end
+
+def get_co2_val(data)
+  # init
+  counts = {'ones' => 0, 'zeroes' => 0 }
+  new_set_zeroes = []
+  new_set_ones = []
+
+  # count all the occurrences
+  (0..data[0].length).each do |bit|
+    data.each_with_index do |val, index|
+      case data[index][bit]
+      when 0
+        counts['zeroes'] += 1
+        new_set_zeroes.push(data[index])
+      when 1
+        counts['ones'] += 1
+        new_set_ones.push(data[index])
+      end
+    end
+    if counts['zeroes'] > counts['ones'] &&
+       counts['ones'] != 0
+      data = new_set_ones
+    else
+      data = new_set_zeroes # 1 is default for oxygen
+    end
+    if data.length == 1
+      return data[0]
+    end
+    #reset counts for next iter
+    new_set_zeroes = []
+    new_set_ones = []
+    counts = {'ones' => 0, 'zeroes' => 0 }
+  end
+  return data[0]
 end
